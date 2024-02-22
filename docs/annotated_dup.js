1e1f405eed93180a5e653c8a85d4c48eebb6247e
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-02-22 13:31:42 -0800

    docs

---
+++ docs/annotated_dup.js
@@ -0,0 +1,12 @@
+var annotated_dup =
+[
+    [ "closed_loop", "namespaceclosed__loop.html", [
+      [ "ClosedLoopController", "classclosed__loop_1_1_closed_loop_controller.html", "classclosed__loop_1_1_closed_loop_controller" ]
+    ] ],
+    [ "encoder_reader", null, [
+      [ "Encoder", "classencoder__reader_1_1_encoder.html", "classencoder__reader_1_1_encoder" ]
+    ] ],
+    [ "motor_control", "namespacemotor__control.html", [
+      [ "MotorDriver", "classmotor__control_1_1_motor_driver.html", "classmotor__control_1_1_motor_driver" ]
+    ] ]
+];
---
+++ docs/namespaces_dup.js
@@ -0,0 +1,11 @@
+var namespaces_dup =
+[
+    [ "closed_loop", "namespaceclosed__loop.html", "namespaceclosed__loop" ],
+    [ "GUI", "namespace_g_u_i.html", [
+      [ "get_user_input", "namespace_g_u_i.html#ae0b50d075d18fb1cb6b4f44f777ee37b", null ],
+      [ "plot_example", "namespace_g_u_i.html#a745007d5c79cd32e46633304fa68d0a0", null ],
+      [ "tk_matplot", "namespace_g_u_i.html#aac66d62be75f7e36dc3773fe7bb71ba8", null ]
+    ] ],
+    [ "main", "namespacemain.html", null ],
+    [ "motor_control", "namespacemotor__control.html", "namespacemotor__control" ]
+];
